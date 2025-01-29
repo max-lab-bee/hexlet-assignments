5c7ff22c519b02d4b60849b4560c516aae2417a7
@hexlet/cli <support@hexlet.io>
2025-01-29 19:40:07 +0500

    submit java-spring-boot-ru/response-entity

--- java-spring-boot-ru/response-entity/src/main/java/exercise/Application.java
+++ java-spring-boot-ru/response-entity/src/main/java/exercise/Application.java
@@ -2,7 +2,6 @@ package exercise;
 
 
 import java.util.List;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.HttpHeaders;
@@ -13,10 +12,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PutMapping;
-
-
 import exercise.model.Post;
-
 import static org.springframework.http.ResponseEntity.ok;
 
 @SpringBootApplication
