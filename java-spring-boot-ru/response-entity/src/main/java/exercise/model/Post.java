abb626085bf8491758d99d327efc008aceec0238
@hexlet/cli <support@hexlet.io>
2025-01-29 19:47:55 +0500

    submit java-spring-boot-ru/response-entity

--- java-spring-boot-ru/response-entity/src/main/java/exercise/model/Post.java
+++ java-spring-boot-ru/response-entity/src/main/java/exercise/model/Post.java
@@ -1,4 +1,4 @@
-package exercise.model;
+/*package exercise.model;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,7 +12,7 @@ import lombok.Setter;
 
 
 
-    public class Post {
+    public class Post{
         private String id;
         private String title;
         private String body;
@@ -47,5 +47,53 @@ import lombok.Setter;
             this.body = body;
         }
     }
+*/
+package exercise.model;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+public class Post { // Отступ 0
+
+    private String id;      // Отступ 4
+    private String title;   // Отступ 4
+    private String body;    // Отступ 4
+
+
+    public Post(String id, String title, String body) {
+        this.id = id;
+        this.title = title;
+        this.body = body;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+}
 
