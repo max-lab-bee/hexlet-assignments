707d22f1790d6fbbb7e6b264d19f159893b3a7c7
@hexlet/cli <support@hexlet.io>
2025-01-29 21:44:07 +0500

    submit java-spring-boot-ru/response-entity_29.01.25_20.43.10

--- java-spring-boot-ru/response-entity_29.01.25_20.43.10/src/main/java/exercise/model/Post.java
+++ java-spring-boot-ru/response-entity_29.01.25_20.43.10/src/main/java/exercise/model/Post.java
@@ -15,30 +15,5 @@ public class Post { // Отступ 0
     private String title;   // Отступ 4
     private String body;    // Отступ 4
 
-
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getBody() {
-        return body;
-    }
-
-    public void setBody(String body) {
-        this.body = body;
-    }
 }
 
