49bd44c7ffccae78ac0992ea9ca7fe5f8e5d5e4f
@hexlet/cli <support@hexlet.io>
2025-02-10 18:09:45 +0300

    submit java-spring-boot-ru/dto

---
+++ java-spring-boot-ru/dto/src/main/java/exercise/dto/CommentDTO.java
@@ -0,0 +1,14 @@
+package exercise.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+
+// BEGIN
+@Setter
+@Getter
+public class CommentDTO {
+    private Long id;
+    private String body;
+}
+// END
